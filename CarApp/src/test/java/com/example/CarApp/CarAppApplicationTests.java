8adee6d8cfc3a0d83d49d5a3f82a68326e46d2cd
Mahmood Younis <younism456@gmail.com>
2018-12-17 20:19:06 +0200

    First commit

---
+++ CarApp/src/test/java/com/example/CarApp/CarAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.CarApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
