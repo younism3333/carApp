9186d0c2cdc83328f9cc7842c1a567eaad93ac80
Mahmood Younis <younism456@gmail.com>
2019-01-11 09:50:39 +0200

    11.01.19

--- CarApp/src/main/java/com/example/CarApp/domain/Trip.java
+++ CarApp/src/main/java/com/example/CarApp/domain/Trip.java
@@ -1,47 +1,56 @@
-//package com.example.CarApp.domain;
-//
-//import java.util.Set;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.ManyToMany;
-//
-//@Entity
-//public class Trip {
-//
-//    @Id
-//    @GeneratedValue(strategy=GenerationType.AUTO)
-//    private int tripId;
-//    private String name;
-//    
-//   @ManyToMany(mappedBy = "trips")
-//   private Set<Car> cars;
-//    
-//    Trip()
-//    {}
-//
-//    public Trip(String name) {
-//        super();
-//        this.name = name;
-//    }
-//
-//    public long getId() {
-//        return tripId;
-//    }
-//
-//    public void setId(int id) {
-//        this.tripId = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//    
-//    
-//}
+package com.example.CarApp.domain;
+
+
+import java.util.Set;
+
+import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Trip {
+
+	  
+	  long trip_id;
+String name;
+
+ 
+private Set<Car> cars;
+
+@ManyToMany(mappedBy = "trips")
+public Set<Car> getCars() {
+	return cars;
+}
+public void setCars(Set<Car> cars) {
+	this.cars = cars;
+}
+
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+public long getTrip_id() {
+	return trip_id;
+}
+public void setTrip_id(long trip_id) {
+	this.trip_id = trip_id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Trip(String name, Set<Car> cars) {
+	super();
+	
+	this.name = name;
+	this.cars=cars;
+}
+
+public Trip(String name) {
+	super();
+	this.name = name;
+	}
+public Trip() {}
+
+}
